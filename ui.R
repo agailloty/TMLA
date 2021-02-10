3ab43fff4f25b5729f782d499f2ab49cbc32cedb
agailloty <mr.clerisaxel@hotmail.fr>
2021-02-10 18:28:30 +0100

    Update UI

--- ui.R
+++ ui.R
@@ -1,18 +1,28 @@
 library(shiny)
 
+
 tagList(
-  
   tags$head(
     tags$title("TMLA - The Machine Learning App"),
-    tags$link(href = "skin/styles.css", rel = "stylesheet")
+    tags$link(rel = "stylesheet", type="text/css", href="www/skin/styles.css"),
+    
   ), 
   
+  tags$header(
+    id = "app-header",
+    h1("TMLA - The Machine Learning App")
+  ),
+  
   navbarPage(
-    id = "Navig",
-    title = h1(" EIVE - Economic Impacts of Volcanic Eruptions"),
-    fluid = TRUE,
-    collapsible = TRUE,
-    
-    tabPanel("Gailloty")
+    title = "TMLA",
+    tabPanel(""),
+    tabPanel("Test", 
+             includeMarkdown("README.Rmd")),
+    tabPanel("Build", 
+             tabsetPanel(
+               tabPanel("Data Import"),
+               tabPanel("Algorithm"),
+               tabPanel("AutoML")
+             ))
   )
 )
